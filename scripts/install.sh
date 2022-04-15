bc1d5325b2608c853d6da07744d1bb948981273a
Valentin Obst <myfruitbasket@protonmail.com>
2022-04-15 17:27:48 +0200

    installing ssh keys

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,12 +15,18 @@ ln -siv ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
 ln -siv ${HOME}/dotfiles/.tmux.conf ${HOME}/.tmux.conf
 echo "--- Initializing empty local configs"
 # git
+echo "${HOME}/.gdbinit_local"
 touch ${HOME}/.gdbinit_local
 # git
+echo "${HOME}/.gitconfig_local"
 touch ${HOME}/.gitconfig_local
 # zsh
+echo "${HOME}/.zsh_local.d/zshrc_local"
 mkdir ${HOME}/.zsh_local.d/ && touch ${HOME}/.zsh_local.d/zshrc_local && \
 mkdir ${HOME}/.zsh_local.d/extensions
 # vim
+echo "${HOME}/.vimrc_local"
 touch ${HOME}/.vimrc_local
+echo "--- Setting up remote access"
+cat ${HOME}/dotfiles/config/ssh/authorized_keys >> ${HOME}/authorized_keys
 echo "--- Done destroying your home"
