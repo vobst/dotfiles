86c43fabaf162fbfdb9fd892ca6cd6580504ef6a
Valentin Obst <myfruitbasket@protomail.com>
2022-12-10 15:19:05 +0100

    use shfmt

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,14 +22,14 @@ MKDIR="mkdir -vp"
 #
 
 function symlink {
-  # $1: target (relative to tree)
-  # $2: location (relative to HOME)
+	# $1: target (relative to tree)
+	# $2: location (relative to HOME)
 
-  TARGET=${HOME}/dotfiles/"$1"
-  LOCATION=${HOME}/"$2"
+	TARGET=${HOME}/dotfiles/"$1"
+	LOCATION=${HOME}/"$2"
 
-  echo "${FUNCNAME[0]}: at $LOCATION to $TARGET"
-  $LN $TARGET $LOCATION
+	echo "${FUNCNAME[0]}: at $LOCATION to $TARGET"
+	$LN $TARGET $LOCATION
 }
 
 #
@@ -37,10 +37,9 @@ function symlink {
 #
 DOTFILES="gdbinit gitconfig gitignore_global zshrc zsh_functions"
 DOTFILES=${DOTFILES}" vimrc tmux.conf xinitrc"
-for dotfile in $DOTFILES
-do
-  symlink "$dotfile" ".$dotfile"
-  touch "${HOME}/.${dotfile}_local"
+for dotfile in $DOTFILES; do
+	symlink "$dotfile" ".$dotfile"
+	touch "${HOME}/.${dotfile}_local"
 done
 
 $MKDIR ${HOME}/.zsh_local.d/
@@ -51,15 +50,13 @@ $MKDIR ${HOME}/.zsh_local.d/extensions
 # Config files: symlinks and local files
 #
 CONFIGDIRS="i3 i3status broot chromium"
-for configdir in $CONFIGDIRS
-do
-  $MKDIR ${HOME}/.config/${configdir}
-  for file in $(find ${PREFIX}/config/${configdir} -maxdepth 1 -type f)
-  do
-    name=$(basename $file)
-    symlink "config/${configdir}/${name}" ".config/${configdir}/${name}"
-  done
-  touch ${HOME}/.config/$configdir/config_local
+for configdir in $CONFIGDIRS; do
+	$MKDIR ${HOME}/.config/${configdir}
+	for file in $(find ${PREFIX}/config/${configdir} -maxdepth 1 -type f); do
+		name=$(basename $file)
+		symlink "config/${configdir}/${name}" ".config/${configdir}/${name}"
+	done
+	touch ${HOME}/.config/$configdir/config_local
 done
 
 #
@@ -67,18 +64,15 @@ done
 #
 $MKDIR ${HOME}/.local/{opt,bin}
 LOCALDIRS="latexindent"
-for localdir in $LOCALDIRS
-do
-  $MKDIR ${HOME}/.local/opt/${localdir}
-  for file in $(find ${PREFIX}/local/opt/${localdir} -maxdepth 1 -type f)
-  do
-    name=$(basename $file)
-    symlink "local/opt/${localdir}/${name}" ".local/opt/${localdir}/${name}"
-    if [[ -x $file ]]
-    then
-      symlink "local/opt/${localdir}/${name}" ".local/bin/${name}"
-    fi
-  done
+for localdir in $LOCALDIRS; do
+	$MKDIR ${HOME}/.local/opt/${localdir}
+	for file in $(find ${PREFIX}/local/opt/${localdir} -maxdepth 1 -type f); do
+		name=$(basename $file)
+		symlink "local/opt/${localdir}/${name}" ".local/opt/${localdir}/${name}"
+		if [[ -x $file ]]; then
+			symlink "local/opt/${localdir}/${name}" ".local/bin/${name}"
+		fi
+	done
 done
 
 #
@@ -87,15 +81,14 @@ done
 $MKDIR ${HOME}/.ssh/
 touch ${HOME}/.ssh/config_local
 symlink "config/ssh/config" ".ssh/config"
-cat ${HOME}/dotfiles/config/ssh/authorized_keys | \
-tee -a ${HOME}/.ssh/authorized_keys > /dev/null
+cat ${HOME}/dotfiles/config/ssh/authorized_keys |
+	tee -a ${HOME}/.ssh/authorized_keys >/dev/null
 
 #
 # vim
 #
 $MKDIR ${HOME}/.vim/ftplugin
 FTPLUGIN="tex"
-for ft in $FTPLUGIN
-do
-  symlink "config/vim/ftplugin/${ft}.vim" ".vim/ftplugin/${ft}.vim"
+for ft in $FTPLUGIN; do
+	symlink "config/vim/ftplugin/${ft}.vim" ".vim/ftplugin/${ft}.vim"
 done
